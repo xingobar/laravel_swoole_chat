88fdb548ab182b08fdf8f101e76e1ec3c954476e
xingobar <gary840227@gmail.com>
2021-10-04 22:11:49 +0800

    feat: 聊天基本架構
    1. 聊天基本架構

--- routes/websocket.php
+++ routes/websocket.php
@@ -15,7 +15,7 @@ use SwooleTW\Http\Websocket\Facades\Websocket;
 
 Websocket::on('connect', function ($websocket, Request $request) {
     // called while socket on connect
-    $websocket->emit('example', '我是伺服器這邊連結成功的訊息');
+    // $websocket->emit('example', '我是伺服器這邊連結成功的訊息');
 });
 
 Websocket::on('disconnect', function ($websocket) {
@@ -25,8 +25,8 @@ Websocket::on('disconnect', function ($websocket) {
 
 Websocket::on('example', function ($websocket, $data) {
     // 群播, 自己不會收到
-    $websocket->broadcast()->emit('message', 'this is a test');
+    $websocket->broadcast()->emit('message', $data);
 
-    // 非群播
-    $websocket->emit('example', 'this is a example channel');
+    // 非群播, 自己收到
+    // $websocket->emit('example', 'this is a example channel');
 });
